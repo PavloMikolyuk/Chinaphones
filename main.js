83c53a5875b1154a103c5c3f92d920ff47fed5d1
PavloMikolyuk <151370571+PavloMikolyuk@users.noreply.github.com>
2025-05-29 19:02:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let navLinks = document.querySelectorAll('.nav-link');
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', (e) => {
-            // Видаляємо клас active з усіх посилань
-            navLinks.forEach(l => l.classList.remove('active'));
-            // Додаємо клас active до клікнутого посилання
-            e.target.classList.add('active');
-        });
-    });
-});
